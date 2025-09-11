1c1be222411b5c738ce28001a7dcedef5d69fb1d
Aníbalcq <alviore@outlock.es>
2025-09-10 20:56:22 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=4, initial-scale=1.0">
+    <title>Servidores</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+   <h1>Administrción de servidores</h1> 
+<h2>Que es un sevidor</h2>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatum doloremque quis explicabo necessitatibus quisquam consectetur, ipsam quam neque natus id repudiandae dignissimos molestiae, earum itaque nihil laboriosam voluptatibus non!Un servidor es una potente computadora en una red diseñada para proveer recursos, servicios y datos a otros dispositivos o clientes dentro de esa red</p>
+Un servidor es una potente computadora en una red diseñada para proveer recursos, servicios y datos a otros dispositivos o clientes dentro de esa red
+</body>
+</html>
