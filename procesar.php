152680080dcc36b23d0c8d4592f5f053e6c59f1a
Aníbalcq <alviore@outlock.es>
2025-09-16 19:42:26 -0500

    nuevoc

--- procesar.php
+++ procesar.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    //recibir la información
+
+    $nombre = $_POST["nombre"];
+
+    $apellido = $_POST["apellido"];
+
+    var_dump($_POST);
+
+    //procesar la información
+
+    $nombre = strtoupper($nombre);
+
+    $apellido = strtolower($apellido);
+
+    //enviar la respuesta
+
+    echo "<h1>Hola $nombre</h1>";
+
+    echo "<h2>Tu apellido es: $apellido</h2>";
+
+} 
+
+ 
+
+?>
